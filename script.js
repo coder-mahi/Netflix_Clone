12cd9d2b1f8ec4cf992bd783bd1caaaf2b728add
MAHESH <maheshshinde9100@gmail.com>
2024-12-07 22:13:35 +0530

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+let signbtn = getElementByClassName('sign-in-btn');
+function signin(){
+    let email = getElementById('mail-input');
+    let pwd = getElementById('pwd-input');
+    if(email.text =="" || pwd.text ==""){
+        window.alert("");
+    }
+}
